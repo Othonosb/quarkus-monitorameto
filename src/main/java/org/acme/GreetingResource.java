98b1500278d63cca1c5edb10408c872728955c81
OTHON-BOTELHO <OTHON-BOTELHO@ibm.com>
2022-10-29 20:34:21 -0300

    Remove unused library

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -6,7 +6,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.net.InetAddress;
 import java.util.Optional;
 
 @Path("/hello")
